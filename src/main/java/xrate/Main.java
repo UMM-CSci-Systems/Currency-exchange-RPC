d35900b6d78e236ae9a0fecae2a313b9a4d94395
Nic McPhee <mcphee@morris.umn.edu>
2021-10-17 23:25:22 -0500

    Fix JavaDoc for `main()`
    I changed this from talking about command line arguments to using prompts.

--- src/main/java/xrate/Main.java
+++ src/main/java/xrate/Main.java
@@ -10,13 +10,13 @@ public class Main {
     private static final String NICS_DUMMY_DATA_URL = "http://facultypages.morris.umn.edu/~mcphee/ExchangeRateData/";
 
     /**
-     * Prompts the user for a currency code, and prints out the
-     * exchange rate for that currency code for the (fixed)
-     * date 25 June 2010 as read from the cached data.
+     * Prompts the user for a currency code, and prints out the exchange rate for
+     * that currency code for the (fixed) date 25 June 2010 as read from the cached
+     * data.
      * 
      * @param args these are ignored
-     * @throws IOException if there are problems reading the exchange
-     * rate data from the URL
+     * @throws IOException if there are problems reading the exchange rate data from
+     *                     the URL
      */
     public static void main(String[] args) throws IOException {
         ExchangeRateReader exchangeRateReader = new ExchangeRateReader(NICS_DUMMY_DATA_URL);
